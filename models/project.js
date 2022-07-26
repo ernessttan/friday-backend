8490b185e655fc2b4ab9c8a136f46f53b29b0b24
Ernest <ernesttancw@gmail.com>
2022-07-25 21:12:16 -0400

    Created project model

---
+++ models/project.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    title: { type: String, required:true },
+    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
